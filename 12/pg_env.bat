274e40b724079960e6cd60a57c25a51cf349e3d6
Trần Văn Luýt <tranvanluyt12b4@gmail.com>
2023-10-12 00:25:21 +0700

    full postgresql

---
+++ 12/pg_env.bat
@@ -0,0 +1,11 @@
+@ECHO OFF
+REM The script sets environment variables helpful for PostgreSQL
+
+@SET PATH="C:\Program Files\PostgreSQL\12\bin";%PATH%
+@SET PGDATA=C:\Program Files\PostgreSQL\12\data
+@SET PGDATABASE=postgres
+@SET PGUSER=postgres
+@SET PGPORT=5432
+@SET PGLOCALEDIR=C:\Program Files\PostgreSQL\12\share\locale
+
+                            
